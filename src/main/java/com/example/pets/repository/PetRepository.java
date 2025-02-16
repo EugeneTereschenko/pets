02bd9ed90d8bf75997bd7574fcf3ca14a444e328
yevhen <tereschenko.eugene@gmail.com>
2025-02-16 14:39:06 +0200

    add file for CI/CD

--- src/main/java/com/example/pets/repository/PetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.pets.repository;
-
-import com.example.pets.model.Pet;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PetRepository extends JpaRepository<Pet, Long> {
-}
